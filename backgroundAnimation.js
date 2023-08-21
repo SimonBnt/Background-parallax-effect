ab98e083619841221e2a495f7e834d775136163b
Your Name <simon.be47@gmail.com>
2023-08-21 23:59:18 +0200

    parallax + left-right anim + animateIcon function() + icon arrangement + css + isCursorInElement function()

--- backgroundAnimation.js
+++ backgroundAnimation.js
@@ -1,4 +1,5 @@
 const icons = document.querySelectorAll(".backgroundIcon")
+const homeSection = document.getElementById("homeSection")
 
 document.addEventListener("mousemove", parallax)
 
@@ -8,8 +9,8 @@ const animateIcon = function(e) {
     const { offsetWidth: width, offsetHeight: height } = icon
 
     const move = 50
-    const xMove = (x / width) * ((move * 2) - move)
-    const yMove = (y / height) * ((move * 2) - move)
+    const xMove = (x / width) * (move * 2)
+    const yMove = (y / height) * (move * 2)
 
     const scaleValue = 1.3
     const resetScaleValue = 1
@@ -24,18 +25,31 @@ const animateIcon = function(e) {
 function parallax(e) {
     const mouseX = e.clientX
     const mouseY = e.clientY
-    
-    icons.forEach(icon => {
-        const speed = parseFloat(icon.getAttribute("data-speed"))
-        const x = (window.innerWidth - mouseX * speed) / 40
-        const y = (window.innerHeight - mouseY * speed) / 40
-        
-        icon.style.setProperty("--parallax-translate-x", `${x}px`)
-        icon.style.setProperty("--parallax-translate-y", `${y}px`)
-    })
+
+    if (isCursorInElement(mouseX, mouseY, homeSection)) {
+        icons.forEach((icon) => {
+            const speed = parseFloat(icon.getAttribute("data-speed"))
+            const x = (window.innerWidth - mouseX * speed) / 50
+            const y = (window.innerHeight - mouseY * speed) / 50
+
+            icon.style.setProperty("--parallax-translate-x", `${x}px`)
+            icon.style.setProperty("--parallax-translate-y", `${y}px`)
+        })
+    } else {
+        icons.forEach((icon) => {
+            icon.style.setProperty("--parallax-translate-x", "0px")
+            icon.style.setProperty("--parallax-translate-y", "0px")
+        })
+    }
 }
 
 icons.forEach(icon => {
     icon.addEventListener("mousemove", animateIcon)
     icon.addEventListener("mouseleave", animateIcon)
 })
+
+function isCursorInElement(x, y, element) {
+    const rect = element.getBoundingClientRect()
+    return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom
+}
+
